f0821fad54d2af9342aafc09ccc34dcc9d0756b4
DanielAlmeidaBrazil <daniel_vip85@hotmail.com>
2023-10-12 16:26:11 -0300

    Adding the project with the Print class.

---
+++ README.md
@@ -0,0 +1,51 @@
+# Javawow! New Java library for a friendly code
+
+Working with Java 17
+
+## What is Javawow?
+
+Javawow is a Java library that allows you to write code in amore friendly
+way. It's a library for new programmers, that are learning Java, but it can be used by anyone. 
+
+## Objective
+
+The main objective of Javawow is to help new programmers to learn Java more easily.
+Create a libraries of a multiporpose methods, that can be used in any Java project.
+Logic, Math, Geometry, Validation, Print, Loop, Tree, Text, Data, File, Connection, etc.
+We're starting working now, so we havea lot of work to do.
+New ideas are welcome. Feel free to contribute.
+
+## We are studens learning Java. We are not experts, but we are trying to help new programmers.
+
+## Join us!
+
+# First package in the Library: br.pops
+
+The package br.pops contains the first classes of the library. We try nomeclatures that can be easily understood by new programmers.
+Our ideia is to work only one package, but we can change this in the future.
+
+
+# First Class in the Library: Print
+
+How to import the class:
+
+```java  
+import static br.com.javawow.lib.Print.*;
+
+/**
+ * For Java 17 
+ */
+class Main {
+    public static void main(String[] args) {
+        print("Hello World!");
+    }
+}
+```
+# Print Class
+
+The Print class provides a series of methods to print messages on the console. The main print method, which simplifies
+the so controversial System.out.println, allows new programmers to print messages with a much simpler call.
+
+We have many ideas to implement in this project, but we need your help.
+
+Thanks for read this.
---
+++ src/main/java/br/com/javawow/lib/Print.java
@@ -0,0 +1,61 @@
+package br.com.javawow.lib;
+
+/**
+ * @author Daniel Almeida
+ * @notes The class Print contains methods to print values.
+ */
+public final class Print {
+
+
+    /**
+     * @notes Exemplo: import static br.org.javaizi.lib.Print.print
+     */
+    public static <T> void  print(T value) {
+        System.out.println(value);
+    }
+
+    /**
+     * @notes The method p() is used to print values in the same line. Similar to System.out.println(value)
+     * @param value
+     * @param <T>
+     */
+    public static <T> void  p(T value) {
+        System.out.println(value);
+    }
+
+    /**
+     * @notes The method pmv() is used to print values in the same line. Similar to System.out.println(message +" "+ value)
+     * @param message
+     * @param value
+     * @param <T>
+     */
+    public static <T> void  pmv(String message, T value) {
+        System.out.println(message + " " + value);
+    }
+
+
+    /**
+     * @notes The method printf() is used to print values in the same line. Similar to System.out.printf(format, args)
+     * @param format
+     * @param args
+     */
+    public static void printf(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+
+    /**
+     * @notes The method pl() is used to print values in the same line. Similar to System.out.print(value)
+     * @param value
+     * @param <T>
+     */
+    public static <T> void  pl(T value) {
+        System.out.print(value + " ");
+    }
+
+    /**
+     * @notes The method breakLine() is used to break a line.
+     */
+    public static void breakLine() {
+        System.out.println();
+    }
+}
